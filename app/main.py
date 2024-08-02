7aa55d12af6a8e47ea43925aee6b0e9ae02e2c38
Przemysław Białoń <przem0.b95@gmail.com>
2024-08-02 23:26:13 +0200

    cleaned main file

--- app/main.py
+++ app/main.py
@@ -7,11 +7,6 @@ from app.response import create_response
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     sock, addr = server_socket.accept()
     print("Connection from: ", addr)
