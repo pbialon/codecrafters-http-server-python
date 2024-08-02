04e47ad94e5f71502d2d645db2f4ad52b2b2d7e3
Przemysław Białoń <przem0.b95@gmail.com>
2024-08-02 23:04:59 +0200

    implemented 200 http response

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
 # Uncomment this to pass the first stage
 import socket
 
+CRLF = '\r\n'
+
+HTTP_VERSION = 'HTTP/1.1'
+STATUS_CODE = 200
+REASON_PHRASE = 'OK'
+
+def create_response():
+    return f'{HTTP_VERSION} {STATUS_CODE} {REASON_PHRASE}{CRLF}{CRLF}'
+
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -9,7 +18,12 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    sock, addr = server_socket.accept()
+    print("Connection from: ", addr)
+    _ = sock.recv(1024)
+    
+    response = create_response()
+    sock.sendall(response.encode())
 
 
 if __name__ == "__main__":
