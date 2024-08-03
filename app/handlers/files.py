9f5c214018ae85c81b87f90666f197049d3f5538
Przemysław Białoń <przem0.b95@gmail.com>
2024-08-03 21:41:42 +0200

    if file doesnt exist

--- app/handlers/files.py
+++ app/handlers/files.py
@@ -2,9 +2,16 @@
 from app.consts import Request, Response, ResponseCode
 from app.server import route
 
+from os.path import exists
+
 
 @route("/files/:filename/")
 class FilesHandler:
     @classmethod
     def get(cls, request: Request, filename: str) -> Response:
+        if not exists(filename):
+            return Response(ResponseCode.NOT_FOUND, [], "")
+        
+        #todo: read file content
+        
         return Response(ResponseCode.NOT_FOUND, [], "")
