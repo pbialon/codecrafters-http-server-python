9b604d7136e422a57be2870b9cd8cdf6cbd0070d
Przemysław Białoń <przem0.b95@gmail.com>
2024-08-03 21:52:25 +0200

    draft of reading a file & returning its contents

--- app/handlers/files.py
+++ app/handlers/files.py
@@ -1,17 +1,28 @@
 
-from app.consts import Request, Response, ResponseCode
+from app.consts import Header, Request, Response, ResponseCode
 from app.server import route
 
-from os.path import exists
+from os.path import exists, join
 
 
 @route("/files/:filename/")
 class FilesHandler:
+    #todo: take it from the flag
+    DIRECTORY = "/tmp/"
+
     @classmethod
     def get(cls, request: Request, filename: str) -> Response:
-        if not exists(filename):
+        filepath = join(cls.DIRECTORY, filename)
+        if not exists(filepath):
             return Response(ResponseCode.NOT_FOUND, [], "")
         
         #todo: read file content
+        with open(filepath, "r") as f:
+            content = f.read()
         
-        return Response(ResponseCode.NOT_FOUND, [], "")
+        headers = [
+            Header("Content-Type", "application/octet-stream"),
+            Header("Content-Length", str(len(content))),
+        ]
+        
+        return Response(ResponseCode.OK, headers, content)
